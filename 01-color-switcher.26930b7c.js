92d988dc6d4dbcb4fe71b9e46c6a854ef51dfda5
Mar1k85 <110822477+Mar1k85@users.noreply.github.com>
2023-01-26 21:43:06 +0000

    Deploying to gh-pages from @ Mar1k85/goit-js-hw-09@d5214767a9e430b8d331cd3a7488ff98e0612cbd 🚀

--- 01-color-switcher.26930b7c.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){intervalId=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.setAttribute("disabled",!0)})),e.addEventListener("click",(function(){clearInterval(intervalId),t.removeAttribute("disabled")}));
-//# sourceMappingURL=01-color-switcher.26930b7c.js.map
--- 01-color-switcher.a7efa856.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){intervalId=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.setAttribute("disabled",!0)})),e.addEventListener("click",(function(){clearInterval(intervalId),t.removeAttribute("disabled")}))}();
-//# sourceMappingURL=01-color-switcher.a7efa856.js.map
